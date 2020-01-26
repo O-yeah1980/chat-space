e20942bd76f7e10d7cc2c274276a8140e2bfe0c1
O-yeah1980 <1980itikyuppa@gmail.com>
2020-01-26 19:40:36 +0900

    JSON get by done method and create HTML

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,44 @@
 $(function(){
+  function buildHTML(message){
+    if ( message.image ) {
+      var html =
+       `<div class="chat-main__messages__message" data-message-id=${message.id}>
+          <div class="chat-main__messages__message__info">
+            <div class="chat-main__messages__message__info__talker">
+              ${message.user_name}
+            </div>
+            <div class="chat-main__messages__message__info__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="chat-main__messages__message__text">
+            <p>
+              ${message.content}
+            </p>
+          </div>
+          <img src=${message.image} >
+        </div>`
+      return html;
+    } else {
+      var html =
+       `<div class="chat-main__messages__message" data-message-id=${message.id}>
+          <div class="chat-main__messages__message__info">
+            <div class="chat-main__messages__message__info__talker">
+              ${message.user_name}
+            </div>
+            <div class="chat-main__messages__message__info__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="chat-main__messages__message__text">
+            <p>
+              ${message.content}
+            </p>
+          </div>
+        </div>`
+      return html;
+    };
+  }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -11,6 +51,9 @@ $(function(){
       processData: false,
       contentType: false
     })
-    console.log(this);
+    .done(function(message) {
+      var html = buildHTML(message);
+      // console.log(message);
+    })
   })
 });
