c9a8b07a9193b840edcb5a8bf2917b0082465002
O-yeah1980 <1980itikyuppa@gmail.com>
2020-02-01 12:59:42 +0900

    ajax when event occured

--- app/assets/javascripts/channels/users.js
+++ app/assets/javascripts/channels/users.js
@@ -2,5 +2,17 @@ $(function(){
   $('#user-search-field').on("keyup", function(){
     let input = $('#user-search-field').val();
     console.log(input);
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      dataType: 'json',
+      data: { keyword: input }
+    })
+    .done(function(users) {
+      console.log("success");
+    })
+    .fail(function() {
+      console.log("failed");
+    })
   });
 });
