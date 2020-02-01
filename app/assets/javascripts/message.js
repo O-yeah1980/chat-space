a71d678d4cad8a17465a6995e6eb2777b7aff8bb
O-yeah1980 <1980itikyuppa@gmail.com>
2020-02-01 21:51:54 +0900

    brush_up_function

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,7 @@
 $(function(){
-  function buildHTML(message){
+  last_message_id = $('.chat-main__messages__message:last').data("message-id");
+  console.log(last_message_id);
+  var buildHTML = function(message){
     if ( message.image ) {
       var html =
        `<div class="chat-main__messages__message" data-message-id=${message.id}>
@@ -63,4 +65,38 @@ $(function(){
       $('.form__submit').prop('disabled', false);
     });
   })
+
+  var reloadMessages = function() {
+    last_message_id = $('.chat-main__messages__message:last').data("message-id");
+    $.ajax({
+      //ルーティングで設定した通りのURLを指定
+      url: "api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        //追加するHTMLの入れ物を作る
+        var insertHTML = '';
+        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        //メッセージが入ったHTMLに、入れ物ごと追加
+        $('.chat-main__messages__message').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      }
+      console.log("success");
+    })
+    .fail(function() {
+      console.log("error");
+    });
+  };
+  // setInterval(reloadMessages, 7000);
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
