e352190e0b1673e9dfd7f6b25693b57546fb6f64
O-yeah1980 <1980itikyuppa@gmail.com>
2020-02-02 11:27:13 +0900

    delete_unneseccary_code

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,9 +1,7 @@
 $(function(){
-  last_message_id = $('.chat-main__messages__message:last').data("message-id");
-  console.log(last_message_id);
-  var buildHTML = function(message){
+  let buildHTML = function(message){
     if ( message.image ) {
-      var html =
+      let html =
        `<div class="chat-main__messages__message" data-message-id=${message.id}>
           <div class="chat-main__messages__message__info">
             <div class="chat-main__messages__message__info__talker">
@@ -22,7 +20,7 @@ $(function(){
         </div>`
       return html;
     } else {
-      var html =
+      let html =
        `<div class="chat-main__messages__message" data-message-id=${message.id}>
           <div class="chat-main__messages__message__info">
             <div class="chat-main__messages__message__info__talker">
@@ -43,8 +41,8 @@ $(function(){
   }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action');
+    let formData = new FormData(this);
+    let url = $(this).attr('action');
     $.ajax({
       url: url,
       type: "POST",
@@ -54,7 +52,7 @@ $(function(){
       contentType: false
     })
     .done(function(message) {
-      var html = buildHTML(message);
+      let html = buildHTML(message);
       $('.chat-main__messages').append(html);
       $('.chat-main__messages').animate({ scrollTop: $('.chat-main__messages')[0].scrollHeight});
       $('form')[0].reset();
@@ -66,36 +64,27 @@ $(function(){
     });
   })
 
-  var reloadMessages = function() {
+  let reloadMessages = function() {
     last_message_id = $('.chat-main__messages__message:last').data("message-id");
     $.ajax({
-      //ルーティングで設定した通りのURLを指定
       url: "api/messages",
-      //ルーティングで設定した通りhttpメソッドをgetに指定
-      type: 'get',
+      type: 'GET',
       dataType: 'json',
-      //dataオプションでリクエストに値を含める
       data: {id: last_message_id}
     })
     .done(function(messages) {
       if (messages.length !== 0) {
-        //追加するHTMLの入れ物を作る
-        var insertHTML = '';
-        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        let insertHTML = '';
         $.each(messages, function(i, message) {
           insertHTML += buildHTML(message)
         });
-        //メッセージが入ったHTMLに、入れ物ごと追加
-        $('.chat-main__messages__message').append(insertHTML);
-        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+        $('.chat-main__messages').append(insertHTML);
+        $('.chat-main__messages').animate({ scrollTop: $('.chat-main__messages')[0].scrollHeight});
       }
-      console.log("success");
     })
     .fail(function() {
-      console.log("error");
     });
   };
-  // setInterval(reloadMessages, 7000);
   if (document.location.href.match(/\/groups\/\d+\/messages/)) {
     setInterval(reloadMessages, 7000);
   }
